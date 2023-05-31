c5bc56958e8f593348ea11181580d6d772b2da99
pierce <piercerhamstine@gmail.com>
2023-05-31 09:05:21 -0500

    Cleaned up, added node default values.

--- datastructure/linkedlist.zig
+++ datastructure/linkedlist.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 
 const Node = struct {
-    value: u32,
-    next: ?*Node,
+    value: u32 = 0,
+    next: ?*Node = null,
 };
 
 const List = struct {
@@ -36,16 +36,13 @@ const List = struct {
 };
 
 pub fn main() void {
-    var linkedList = List{ .head = null };
+    var linkedList = List{};
 
-    var n1 = Node{
-        .value = 0,
-        .next = null,
-    };
+    var n1 = Node{};
+    var n2 = Node{ .value = 1 };
 
     linkedList.push(&n1);
+    linkedList.push(&n2);
 
     linkedList.printList();
-
-    std.debug.print("{s}", .{"Hello World"});
 }
